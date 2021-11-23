c3a76c2ca0731d3de6e58cf24f99a118577cb5a9
CrUeLGeRoI <elbg2016@gmail.com>
2021-11-23 23:16:23 +0200

    add findMaxNumOddColumn and findMaxNumOddRow

--- src/TwoDimArray.java
+++ src/TwoDimArray.java
@@ -7,8 +7,12 @@ public class TwoDimArray {
         System.out.println(Arrays.deepToString(a));
         OptionalInt b = findMaxNumDiagonal(a);
         System.out.println(b.getAsInt());
-        OptionalInt c = findMaxNumOddRow(a);
-        System.out.println(c.getAsInt());
+//        OptionalInt c = findMaxNumDiagonalReverse(a);
+//        System.out.println(c.getAsInt());
+        OptionalInt d = findMaxNumOddRow(a);
+        System.out.println(d.getAsInt());
+        OptionalInt e = findMaxNumOddColumn(a);
+        System.out.println(e.getAsInt());
     }
     static int[][] createArray(int sizeArrayA, int sizeArrayB){
         //sizeArrayA = rows
@@ -23,25 +27,74 @@ public class TwoDimArray {
         return array;
     }
     static OptionalInt findMaxNumDiagonal(int[][] array){
-        int[] calcArray = new int[array.length * 2];
+        int[] calcArray = new int[array.length];
         for (int i = 0; i < array.length; i++){
             calcArray[i] = array[i][i];
+        }
+        System.out.println("findMaxNumDiagonal");
+        for (int i = 0; i < array.length; i++) {  //идём по строкам
+            for (int j = 0; j < array[0].length; j++) {//идём по столбцам
+                System.out.print(" " + array[i][j] + " "); //вывод элемента
+            }
+            System.out.println();//перенос строки ради визуального сохранения табличной формы
         }
         OptionalInt max = Arrays.stream(calcArray).max();
         max.getAsInt();
         return max;
     }
-    static OptionalInt findMaxNumOddRow(int[][] array){
-//        int[] calcArray = new int[6];
-//        for (int i = 1; i < array.length; i += 2){
-//            for (int j = 0; j < array[0].length; j++){
-//                calcArray[j] = array[i][j];
-//            }
+//    static OptionalInt findMaxNumDiagonalReverse(int[][] array){
+//        int[] calcArray = new int[array.length];
+//        for (int i = 0; i < array.length; i++){
+//            calcArray[i] = array[i][i];
 //        }
-//        System.out.println(Arrays.toString(calcArray));
-        return OptionalInt.of(0);
-    }
-//    static int[] findMaxNumOddColumn(){
+//        System.out.println("findMaxNumDiagonalReverse");
+//        for (int i = 0; i < array.length; i++) {  //идём по строкам
+//            for (int j = 0; j < array[0].length; j++) {//идём по столбцам
+//                System.out.print(" " + array[i][j] + " "); //вывод элемента
+//            }
+//            System.out.println();//перенос строки ради визуального сохранения табличной формы
+//        }
 //
+//        OptionalInt max = Arrays.stream(calcArray).max();
+//        max.getAsInt();
+//        return max;
 //    }
+    static OptionalInt findMaxNumOddRow(int[][] array){
+        int index = 0;
+        int[] calcArray = new int[(array.length / 2) * array[0].length];
+        for (int i = 0; i < array.length; ++i){
+            for (int j = 0; j < array[0].length; ++j){
+                if (i % 2 == 0 || i == 0){
+                    continue;
+                }
+                else{
+                    calcArray[index++] = array[i][j];
+                }
+            }
+        }
+        System.out.println("findMaxNumOddRow");
+        System.out.println(Arrays.toString(calcArray));
+        OptionalInt max = Arrays.stream(calcArray).max();
+        max.getAsInt();
+        return max;
+    }
+    static OptionalInt findMaxNumOddColumn(int[][] array){
+        int index = 0;
+        int[] calcArray = new int[(array[0].length / 2) * array.length];
+        for (int i = 0; i < array[0].length; ++i){
+            for (int j = 0; j < array.length; ++j){
+                if (j % 2 == 0 || j == 0){
+                    continue;
+                }
+                else{
+                    calcArray[index++] = array[i][j];
+                }
+            }
+        }
+        System.out.println("findMaxNumOddColumn");
+        System.out.println(Arrays.toString(calcArray));
+        OptionalInt max = Arrays.stream(calcArray).max();
+        max.getAsInt();
+        return max;
+    }
 }
